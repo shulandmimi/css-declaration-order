552354e73403e45480426ab41c946754d7f12685
shulandmimi <sshuang141@163.com>
2022-10-09 16:49:20 +0800

    feat(cssorder): finished css syntax & simple cli run

--- packages/cssorder/src/main.rs
+++ packages/cssorder/src/main.rs
@@ -1,11 +1,8 @@
-#![feature(fs_try_exists)]
 #![feature(is_some_with)]
-use std::{
-    collections::HashMap,
-    fs::{read, try_exists},
-    path::PathBuf,
-    str::FromStr,
-};
+#![warn(dead_code)]
+
+use glob::glob;
+use std::{collections::HashMap, fs::read, path::PathBuf};
 
 use clap::Parser;
 use cssorder::parser::css::Config;
@@ -19,9 +16,9 @@ struct Cli {
     paths: Vec<String>,
 }
 
-fn exists(path: &PathBuf) -> bool {
-    return try_exists(path).is_ok_and(|ok| *ok);
-}
+// fn exists(path: &PathBuf) -> bool {
+//     return try_exists(path).is_ok_and(|ok| *ok);
+// }
 
 fn main() {
     let args = Cli::parse();
@@ -30,33 +27,25 @@ fn main() {
         return;
     }
 
-    let filename = PathBuf::from_str(args.paths[0].as_str()).unwrap();
-    if !exists(&filename) {
-        return;
-    };
-
-    // let filename = PathBuf::from_str("../../examples/css/index.css").unwrap();
-
-    let content = read(filename).unwrap();
-
-    let content = String::from_utf8_lossy(content[..].into()).to_string();
-
-    let mut parser = raffia::Parser::new(&content, Syntax::Css);
-
-    let mut ast = parser.parse::<Stylesheet>().unwrap();
+    let filenames: Vec<PathBuf> = args
+        .paths
+        .into_iter()
+        .map(|path| {
+            glob(&path).expect(format!("not match files, please check path {}", path).as_str())
+        })
+        .map(|paths| {
+            paths.filter_map(|path| match path {
+                Ok(filename) => Some(filename),
+                Err(_) => None,
+            })
+        })
+        .flatten()
+        .collect();
 
     let mut key_weight_map = HashMap::new();
 
     key_weight_map.insert("width".to_string(), 2000);
     key_weight_map.insert("height".to_string(), 1999);
-
-    cssorder::parser::css::run(
-        &mut ast,
-        Config {
-            weight_map: Some(key_weight_map),
-            default_weight: Some(1000),
-        },
-    );
 
     let stdout = std::io::stdout();
 
@@ -66,5 +55,22 @@ fn main() {
 
     let mut code_gen = CodeGenerator::new(writer, css_serialize);
 
-    code_gen.emit(&mut ast).unwrap();
+    for file in filenames {
+        let content = read(file).unwrap();
+
+        let content = String::from_utf8_lossy(content[..].into()).to_string();
+
+        let mut parser = raffia::Parser::new(&content, Syntax::Css);
+        let mut ast = parser.parse::<Stylesheet>().unwrap();
+
+        cssorder::parser::css::run(
+            &mut ast,
+            Config {
+                weight_map: Some(key_weight_map.clone()),
+                default_weight: Some(1000),
+            },
+        );
+
+        code_gen.emit(&mut ast).unwrap();
+    }
 }
